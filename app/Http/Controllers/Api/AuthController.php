18a17aac779bb93506a767f9117eaf6a12bc99a5
bayufius <58999645+bayufazri@users.noreply.github.com>
2021-07-10 22:49:45 +0700

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,47 +4,61 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-    $validatedData = $request->validate([
-    'name' => 'required|max:55',
-    'email' => 'email|required|unique:users',
-    'npp' => 'required',
-    'npp_supervivor' => 'required',
-    'password' => 'required|confirmed'
-  ]);
+    public $successStatus = 200;
 
-  $validatedData['password'] = bcrypt($request->password);
-  $user = User::create($validatedData);
-  $accessToken = $user->createToken('authToken')->accessToken;
+    public function login(){
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('nApp')->accessToken;
+            return response()->json(['success' => $success], $this->successStatus);
+        }
+        else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
 
-  return response([ 'user' => $user, 'access_token' => $accessToken]);
-}
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'npp' => 'required',
+            'npp_supervivor' => 'required',
+            'password' => 'required'
+        ]);
 
-public function login(Request $request) {
-  $loginData = $request->validate([
-    'email' => 'email|required',
-    'password' => 'required'
-  ]);
-  
-  if (!auth()->attempt($loginData)) {
-    return response(['message' => 'Invalid Credentials']);
-  }
- 
-  $accessToken = auth()->user()->createToken('authToken')->accessToken;
-  return response(['user' => auth()->user(), 'access_token' => $accessToken]);
-}
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
 
-public function logout(Request $request) {
-  $request->user()->token()->revoke();
-  return response()->json([
-    'message' => 'Successfully logged out'
-  ]);
-}
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('nApp')->accessToken;
+        $success['name'] =  $user->name;
 
-public function user(Request $request) { 
-  return response()->json($request->user());
-}
+        return response()->json(['success'=>$success], $this->successStatus);
+    }
+
+    public function logout(Request $request)
+    {
+        $logout = $request->user()->token()->revoke();
+        if($logout){
+            return response()->json([
+                'message' => 'Successfully logged out'
+            ]);
+        }
+    }
+
+    public function details()
+    {
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus);
+    }
 }
